c03328465c0adc549936167581c7eceb4a1e6183
Tabish <tabdhsan11@gmail.com>
2024-06-28 02:32:31 -0400

    updated code to account for multiple instances of the same investment

--- stock_crypto_telegram_bots.py
+++ stock_crypto_telegram_bots.py
@@ -21,7 +21,7 @@ class TelegramBots:
         self.records = self.google_sheets_wrapper.get_all_records()
         self.investment_dict = self.get_investment_dict(self.records)
 
-    # Checks if stock market is open based on daily hours (doesn't acct for edge cases like holidays)
+    # Checks if stock market is clear based on daily hours (doesn't acct for edge cases like holidays)
     # This can probably be done via a simple api call but I don't need robust data
     def market_open_checker(given_date_time):
         START_TIME = time(9, 30, 0)
@@ -39,11 +39,18 @@ class TelegramBots:
 
     def get_investment_dict(self, records: dict):
         investment_dict = {}
-        for i in records:
-            # Turns the 'Investment_Name' column from gsheets to the key of a new dictionary with the old keys as values
-            investment_name = i["Investment_Name"]
-            del i["Investment_Name"]
-            investment_dict[investment_name] = i
+        for record in records:
+            investment_name = record["Investment_Name"]
+            if investment_name not in investment_dict:
+                investment_dict[investment_name] = []
+
+            condition = {
+                "Investment_Type": record["Investment_Type"],
+                "Target_Price": record["Target_Price"],
+                "Comparison_Type": record["Comparison_Type"],
+            }
+
+            investment_dict[investment_name].append(condition)
         return investment_dict
 
     def get_stock_price(self, investment_name):
@@ -61,13 +68,13 @@ class TelegramBots:
         return False
 
     def get_dict_by_type(self, investment_type) -> dict:
-        # Pull out all stocks from investment dict
-
         filtered_dict = {}
-        for key, val in self.investment_dict.items():
-            if val["Investment_Type"] == investment_type:
-                filtered_dict[key] = val
-
+        for key, conditions in self.investment_dict.items():
+            filtered_conditions = [
+                c for c in conditions if c["Investment_Type"] == investment_type
+            ]
+            if filtered_conditions:
+                filtered_dict[key] = filtered_conditions
         return filtered_dict
 
     def get_dt_string(self):
@@ -93,23 +100,27 @@ class TelegramBots:
 
         crypto_prices = self.get_price_for_all_cryptos(crypto_list)
 
-        for crypto_name in crypto_dict:
+        for crypto_name, conditions in crypto_dict.items():
             current_price = crypto_prices[crypto_name.lower()]["usd"]
-            target_price = crypto_dict[crypto_name]["Target_Price"]
-            comparison_type = crypto_dict[crypto_name]["Comparison_Type"]
 
-            # Do a price comparison
-            is_valid_price = self.is_valid_price(
-                current_price,
-                target_price,
-                comparison_type,
-            )
+            for condition in conditions:
+                target_price = condition["Target_Price"]
+                comparison_type = condition["Comparison_Type"]
 
-            # Send a message
-            if is_valid_price is True:
-                message_text = self.get_message_text(crypto_name, current_price)
-                self.telegram_wrapper.send_message("crypto", message_text)
+                # Do a price comparison
+                is_valid_price = self.is_valid_price(
+                    current_price,
+                    target_price,
+                    comparison_type,
+                )
 
-                print(f"SENDING CRYPTO MSG NOW for {crypto_name}")
-            else:
-                print(f"NO CRYPTO MSG for {crypto_name}")
+                # Send a message
+                if is_valid_price is True:
+                    message_text = self.get_message_text(crypto_name, current_price)
+                    message_text += f" | Condition: {comparison_type} {target_price}"
+
+                    self.telegram_wrapper.send_message("crypto", message_text)
+
+                    print(f"SENDING CRYPTO MSG NOW for {crypto_name}")
+                else:
+                    print(f"NO CRYPTO MSG for {crypto_name}")
